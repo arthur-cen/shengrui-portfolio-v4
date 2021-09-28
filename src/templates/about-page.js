8ac344251364f7b145b2ff678eddbb23fb66670d
Arthur <cenrui.93@gmail.com>
2021-09-27 21:44:17 -0700

    added a button to download resume

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,7 +1,9 @@
-import React from "react"
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
 import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
+import { RiArrowRightSLine } from "react-icons/ri"
 import SEO from "../components/seo"
 
 export const pageQuery = graphql`
@@ -14,12 +16,17 @@ export const pageQuery = graphql`
         title
       }
     }
+		site {
+			siteMetadata {
+				resumePath: resume
+			}
+  	}
   }
 `
 const AboutPage = ({ data }) => {
-	const { markdownRemark } = data // data.markdownRemark holds your post data
+	const { markdownRemark, site } = data // data.markdownRemark holds your post data
   const { frontmatter, html, excerpt } = markdownRemark
-
+  const { resumePath } = site.siteMetadata
 	return (
 		<Layout className="page">
 			<SEO
@@ -29,6 +36,15 @@ const AboutPage = ({ data }) => {
 			<div className="wrapper">
 				<h1>{frontmatter.title}</h1>
 				<article dangerouslySetInnerHTML={{ __html: html }} />
+				<a href={resumePath} 
+					download 
+					className="button"
+					sx={{
+						variant: 'links.button'
+					}}>
+						Resume Download
+						<span class="icon -right"><RiArrowRightSLine/></span>
+					</a>
 			</div>
 		</Layout>
 	)
